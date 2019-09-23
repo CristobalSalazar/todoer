296a6757b6ca0cdbf9806198b6d9c0b4e0e1f61f
chris <chris.salza@gmail.com>
2019-09-23 01:03:02 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonToggleComponent } from './components/button-toggle/button-toggle.component';
+// Material
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material'
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+// Firebase
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { environment } from "../environments/environment";
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    ButtonToggleComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTabsModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+	firebase : {
+	  apiKey: "AIzaSyCSwbL3bl5wcuXd_WNhGej4JZyLGVhr3Cs",
+	  authDomain: "todoer-fd917.firebaseapp.com",
+	  databaseURL: "https://todoer-fd917.firebaseio.com",
+	  projectId: "todoer-fd917",
+	  storageBucket: "todoer-fd917.appspot.com",
+	  messagingSenderId: "622740743061",
+	  appId: "1:622740743061:web:06df5e005e161d1565cc9c"
+	} 
+
 };
